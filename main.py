b4f9873932698b67a1a644c5ab49b8074f6a29c4
gekkearg3ntijn <justinprogrammeren@gmail.com>
2023-10-26 15:13:18 +0200

    dingen

--- main.py
+++ main.py
@@ -133,7 +133,7 @@ while run:
       score[0] += 1
       round_over = True
       round_over_time = pygame.time.get_ticks()
-  else:
+  else: 
     #display victory image
     screen.blit(victory_img, (460, 350))
     if pygame.time.get_ticks() - round_over_time > ROUND_OVER_COOLDOWN:
